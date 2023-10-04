8976e52ebb92313bb994d442a9981097173b7c3a
kongukjae <kongukjae@gmail.com>
2023-10-04 16:50:02 +0900

    동적으로 태그 처리할 자료구조 셋업 기본 구조

---
+++ element-structure-test.js
@@ -0,0 +1,17 @@
+let elementStructure = {
+  tagName : "li",
+  content : {
+    emoji : "emoji",
+    order : 1,
+    price : 1000,
+  },
+  styles : {
+    border: "",
+    padding: "",
+    display: "",
+    width:"",
+    height:"",
+    color:"",
+    backgroundColor:"",
+  }
+}
