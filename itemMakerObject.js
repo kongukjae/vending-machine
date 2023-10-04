da502f54a081fdf1581d51ebba71b7be4e851e3b
kongukjae <kongukjae@gmail.com>
2023-10-04 15:23:35 +0900

    주요 기능 의사코드 정리 item 부분

--- itemMakerObject.js
+++ itemMakerObject.js
@@ -6,11 +6,21 @@
 //   }
 // ]
 
-function vendingMachineItemMakerObject(order, emoji, price) {
+/**
+ * 
+ * ? 1. order : 배열의 크기만큼
+ * ? 2. emoji : 랜덤으로 결정
+ * ? 3. price : 랜덤으로 결정
+ * ? 4. isClick : 기본값 False
+ * 
+ */
+
+function vendingMachineItemMakerObject(order, emoji, price, clickState) {
   return {
     order:order,
     emoji:emoji,
     price:price,
+    isClick:clickState,
   }
 }
 
