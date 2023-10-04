504250184296361afb700b51dbb5bd5247d6d6f5
kongukjae <kongukjae@gmail.com>
2023-10-04 15:16:35 +0900

    프로젝트 스타트!

---
+++ itemMakerObject.js
@@ -0,0 +1,16 @@
+// const dummyItems = [
+//   {
+//     order:0,
+//     emoji: "",
+//     price: 1000,
+//   }
+// ]
+
+function vendingMachineItemMakerObject(order, emoji, price) {
+  return {
+    order:order,
+    emoji:emoji,
+    price:price,
+  }
+}
+
