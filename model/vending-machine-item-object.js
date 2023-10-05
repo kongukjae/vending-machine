77138d720cff699912b141c536673b84644c4f51
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-10-05 21:19:33 +0900

    자판기 아이템을 위한 아이템 매핑 모듈 초안

---
+++ model/vending-machine-item-object.js
@@ -0,0 +1,10 @@
+module.exports = function (id, name, price, quantity) {
+  function objectMap(id, name, price, quantity) {
+    return {
+      id: id,
+      name: name,
+      price: price,
+      quantity: quantity
+    }
+  }
+}
